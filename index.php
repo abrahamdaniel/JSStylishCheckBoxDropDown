db2da659dfc1558c65fd0a91f9f2886d316f6e97
Abraham Daniel <abraham@twag.co>
2013-01-26 22:43:46 +0530

    initial

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+</head>
+
+<body>
+
+<link rel="stylesheet" href="css/form.css" type="text/css" />
+<script type="text/javascript" src="js/custom-form-elements.js"></script>
+<center>
+<h1>Stylish Checkbox</h1>
+    <input type="checkbox" name="a" class="styled" />
+
+<br /><br /><br /><br />
+<h1>Stylish Radio Button</h1>
+    <input type="radio" name="radio" class="styled" /> 
+    <input type="radio" name="radio" class="styled" /> 
+
+<br /><br /><br /><br />
+<h1>Stylish DropDown</h1>
+    <select name="d" class="styled" style="height:31px; ">
+      <option value="1" style="height:25px;font-size:16px;">Option 1</option>
+      <option value="2" style="height:25px;font-size:16px;">Option 2</option>
+      <option value="3" style="height:25px;font-size:16px;">Option 3</option>
+    </select>
+</center>
+</body>
+</html>
